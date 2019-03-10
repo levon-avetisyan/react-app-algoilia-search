4e278ee78b1ce1bb91c5ea968f4cc299a3c791cc
levon-avetisyan <levon.s.avetisyan@gmail.com>
2019-03-10 20:42:07 +0400

    Updates

---
+++ precache-manifest.3129a20b6e3c3bb952349544d14d5492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b41af9a7ea4a20903e6d",
+    "url": "/react-app-algoilia-search/static/js/runtime~main.df88d85c.js"
+  },
+  {
+    "revision": "e8316e59a8a110b1d1e9",
+    "url": "/react-app-algoilia-search/static/js/main.7d505032.chunk.js"
+  },
+  {
+    "revision": "cef3defa517c39e63d01",
+    "url": "/react-app-algoilia-search/static/js/2.8942bb20.chunk.js"
+  },
+  {
+    "revision": "e8316e59a8a110b1d1e9",
+    "url": "/react-app-algoilia-search/static/css/main.9dbb1217.chunk.css"
+  },
+  {
+    "revision": "553c8f6f705f632324329f075a1cfa49",
+    "url": "/react-app-algoilia-search/index.html"
+  }
+];
